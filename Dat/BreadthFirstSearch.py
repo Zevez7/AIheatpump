64c01931fe655ebadd20337a1b950d88ba29d3d5
Zevez7 <datguy7@gmail.com>
2022-11-24 13:36:10 -0500

    bfs completed

--- Dat/BreadthFirstSearch.py
+++ Dat/BreadthFirstSearch.py
@@ -1,10 +1,10 @@
 import Queue
 
 
-dataStore = {"a": ["b","d"],
+dataStore = {"a": ["b", "d"],
              "b": ["c"],
              "c": ["d"],
-             "d": ["e"]
+             "d": ["e"],
              }
 
 
@@ -28,10 +28,30 @@ def breadthFirstSearch(data):
 
         # store current node and remove from frontier
         currentNode = frontier.pop()
-        print(currentNode)
+        print("currentNode", currentNode)
+        if endState == currentNode["state"]:
+            print("endstate reached terminated program")
+            return
 
+        # add the explored state
         exploredSet.append(currentNode["state"])
         print("exploredSet", exploredSet)
+
+        # get the next state
+        state = dataStore[currentNode["state"]]
+        print(state)
+        for item in state:
+            nextNode = {"state": item, "prevNode": currentNode["state"]}
+            print(nextNode)
+            # check to see if it's not already visited
+            if nextNode["state"] not in exploredSet:
+                print("not in state pushing", nextNode)
+                frontier.push(nextNode)
+
+        # print(frontier)
+        # print(item)
+        # print(state)
+    # frontier.push()
 
 
 def main():
